79f25cd81d407cbb6cd524ac8a4c87ca4403a0d1
Vilius <vilius.lt@gmail.com>
2014-05-10 21:19:13 +0300

    Code cleanup: and results are shown

--- htmlAnalyzer.js
+++ htmlAnalyzer.js
@@ -3,25 +3,25 @@ function Analyzer(browsers) {
     this._browsers = browsers;
     this._documentTags = [];
 
-    var collectDocumentTags = function(parent) {
-        parent = typeof parent !== 'undefined' ? parent : document.getElementsByTagName('body')[0];
+    var collectDocumentTags = function(analyzer, parent) {
+        parent = typeof parent !== 'undefined' ? parent : document.querySelector('body');
 
-        if (parent.tagName !== 'undefined' && this._documentTags.indexOf(parent.tagName.toLowerCase()) === -1) {
-            this._documentTags.push(parent.tagName.toLowerCase());
+        if (parent.tagName !== 'undefined' && analyzer._documentTags.indexOf(parent.tagName.toLowerCase()) === -1) {
+            analyzer._documentTags.push(parent.tagName.toLowerCase());
         }
 
         var children = parent.children;
 
         if (children.length > 0) {
             for (var id = 0; id < children.length; id++) {
-                collectDocumentTags(children[id]);
+                collectDocumentTags(analyzer, children[id]);
             }
         }
     };
 
     this.getDocumentTags = function() {
         if (this._documentTags.length === 0) {
-            collectDocumentTags();
+            collectDocumentTags(this);
         }
 
         return this._documentTags;
@@ -33,7 +33,7 @@ function Analyzer(browsers) {
 
         for (var id = 0; id < this._browsers.length; id++) {
             var name = this._browsers[id]._name;
-            var version = this._browsers[id].getLowestSupportedVersionForKnownTags(tags); //TODO: maybe use function for all tags
+            var version = this._browsers[id].getLowestSupportedVersionForKnownTags(tags);
 
             supportedBrowsers.push(new BrowserInfo(name, version));
         }
@@ -176,10 +176,10 @@ function Browser(name, versions) {
 //Version
 function BrowserVersion(identifier, tags) {
     this._identifier = identifier;
-    this._tags = tags;
+    this._tags = [];
 
     for (var id = 0; id < tags.length; id++) {
-        this._tags.push(tags[id].toLowerCase());
+        this._tags.push(tags[id]._tag.toLowerCase());
     }
 
     this.isSupportFromThisVersion = function(tag) {
@@ -188,110 +188,31 @@ function BrowserVersion(identifier, tags) {
 }
 
 
-//INFO apie tai, kokoiose narsyklese ir ju versijose prieinamas puslapis pvz 
-/*
-
-IE9, Chrome5 ff9
-
-div ie4, chrome 1, ff 1
-audio ie9, chrome5, ff9
-video ie8, chrome5, ff7
-
-*/
-
+//Tag
 var Tag = function(tag, attributes, isNew) {
     this._tag = tag.toLowerCase();
     this._attributes = attributes || [];
     this._isNew = isNew || false;
-}
+};
 
+
+//Tag attribute
 var TagAttribute = function(attribute, values, isNew){
     this._attribute = attribute.toLowerCase();
     this._values = values || [];
     this._isNew = isNew || false;
-
 };
 
+
+//Tag attribute value
 var TagAttributeValue = function(value, isNew) {
     this._value = value;
     this._isNew = isNew || false;
-}
-
-(function(w) {
-
-
-    //
-    var ie8 = new BrowserVersion('8.0.1', ['script', 'div']);
-    var ie9 = new BrowserVersion('9.0', ['div', 'VidEO', 'canvas', 'audio', 'article']);
-    var ie10 = new BrowserVersion('10.1.0.1', ['canvas']);
-    var ie = new Browser('Internet Explorer', [ie8, ie9, ie10]);
-
-
-    var chrome3 = new BrowserVersion('3.0', ['script', 'audio', 'div']);
-    var chrome4 = new BrowserVersion('4.0', ['canvas', 'script', 'audio', 'div']);
-    var chrome6 = new BrowserVersion('6.0', ['script', 'audio', 'div', 'article']);
-    var chrome12 = new BrowserVersion('12.2.2.', ['div', 'VidEO', 'canvas']);
-    var chrome15 = new BrowserVersion('10.1.0.1', ['canvas']);
-    var chrome = new Browser('Google Chrome', [chrome3, chrome4, chrome6, chrome12, chrome15]);
-
-
-    var ff2 = new BrowserVersion('2.0', ['canvas', 'script', 'audio', 'div']);
-    var ff35 = new BrowserVersion('3.5', ['script', 'audio', 'div']);
-    var ff4 = new BrowserVersion('4.0', ['script', 'audio', 'div', 'article']);
-    var ff = new Browser('Mozilla Firefox', [ff2, ff35, ff4]);
-    //TODO: allow order to be non sequential and create versions order. So it would be possilbe to know that 8.0.1 < 10.1.01
-
-
-    console.log(ie._name);
-    console.log(ie9.isSupportFromThisVersion('auDIO'));
-    console.log(ie.getVersionsIdentifiers());
-    console.log(ie.getTags());
-    console.log(ie.whenSupportAddedFor('canvas'));
-
-
-    //TODO: print where is the tag supported
-
-    //Instantenious function body:
-    var analyzer = new Analyzer([ie, chrome, ff]);
-
-    var tags = analyzer.getDocumentTags();
-    console.log('your website can work in lowest version of IE (all tags):' + ie.getLowestSupportedVersionForAllTags(tags));
-    console.log('your website can work in lowest version of IE (known tags only):' + ie.getLowestSupportedVersionForKnownTags(tags));
-
-    for (var tagId = 0 ; tagId < tags.length; tagId++) {
-        console.log(tags[tagId]);
-    }
-
-    var browsersInfo = analyzer.getBrowserSupportInfo();
-
-    console.log('This page is available in these browsers:');
-    for (var browserInfoId = 0; browserInfoId < browsersInfo.length; browserInfoId++) {
-        console.log(browsersInfo[browserInfoId]._name + ' ' + browsersInfo[browserInfoId]._version + '+');
-    }
-
-    var tagsSupportInfo = analyzer.getBrowserSupportInfoForEachTag();
-    for (var id = 0; id < tagsSupportInfo.length; id++) {
-        var browserInfo = '|';
-
-        for (var browserId = 0; browserId < tagsSupportInfo[id]._browsersInfo.length; browserId++) {
-            browserInfo += tagsSupportInfo[id]._browsersInfo[browserId]._name + ' ' + tagsSupportInfo[id]._browsersInfo[browserId]._version + '|';
-        }
-
-
-        console.log(tagsSupportInfo[id]._tag + ': ' + browserInfo);
-
-
-    }
+};
 
 
 
-
-
-
-
-
-    //browser tags turėtų saugoti objektus iš tag'o ir jo palaikomų atributų su reikšmėmis. pvz
-
+(function() {
     //Internet Explorer
     var sectionTagIe9 = new Tag('section', [], true);
     var articleTagIe9 = new Tag('article', [], true);
@@ -300,15 +221,17 @@ var TagAttributeValue = function(value, isNew) {
     var footerTagIe9 = new Tag('footer', [], true);
     var canvasTagIe9 = new Tag('canvas', [], true);
     var videoTagIe9 = new Tag('video', [new TagAttribute('type',  [new TagAttributeValue('video/mp4', true)], true)], true);
-    var audioTagIe9 = new Tag('audio', [new TagAttribute('type', [new TagAttributeValue('audio/mpeg', true)], true)], true)
+    var audioTagIe9 = new Tag('audio', [new TagAttribute('type', [new TagAttributeValue('audio/mpeg', true)], true)], true);
     var svgTagIe9 = new Tag('svg', [], true);
     var ie9 = new BrowserVersion('9.0', [sectionTagIe9, articleTagIe9, asideTagIe9, headerTagIe9, footerTagIe9, canvasTagIe9, videoTagIe9, audioTagIe9, svgTagIe9]);
 
     var inputTagIe10 = new Tag('input', [
         new TagAttribute('type', [new TagAttributeValue('range', true), new TagAttributeValue ('number', true)]),
-        new TagAttribute('placeholder', [], true)]);
+        new TagAttribute('placeholder', [], true),
+        new TagAttribute('required', [], true)]);
     var progressTagIe10 = new Tag('progress', [], true);
-    var ie10 = new BrowserVersion('10.0', [inputTagIe10, progressTagIe10]);
+    var datalistTagIe10 = new Tag('datalist', [], true);
+    var ie10 = new BrowserVersion('10.0', [inputTagIe10, progressTagIe10, datalistTagIe10]);
 
     var ie = new Browser('Microsoft Internet Explorer', [ie9, ie10]);
 
@@ -320,27 +243,37 @@ var TagAttributeValue = function(value, isNew) {
     var audioTagFirefox3_5 = new Tag('audio', [new TagAttribute('type', [new TagAttributeValue('audio/ogg', true), new TagAttributeValue('audio/wav', true)], true)], true);
     var firefox3_5 = new BrowserVersion('3.5', [videoTagFirefox3_5, audioTagFirefox3_5]);
 
-    var inputTagFirefox4 = new Tag('input', new TagAttribute('placeholder',[], true), false);
+    var inputTagFirefox4 = new Tag('input', [new TagAttribute('placeholder',[], true), new TagAttribute('required', [], true)], false);
     var sectionTagFirefox4 = new Tag('section', [], true);
     var articleTagFirefox4 = new Tag('article', [], true);
     var asideTagFirefox4 = new Tag('aside', [], true);
     var headerTagFirefox4 = new Tag('header', [], true);
     var footerTagFirefox4 = new Tag('footer', [], true);
     var svgTagFirefox4 = new Tag('svg', [], true);
-    var firefox4 = new BrowserVersion('4.0', [sectionTagFirefox4, articleTagFirefox4, asideTagFirefox4, headerTagFirefox4, footerTagFirefox4, svgTagFirefox4, inputTagFirefox4]);
+    var datalistTagFirefox4 = new Tag('datalist', [], true);
+    var firefox4 = new BrowserVersion('4.0', [sectionTagFirefox4, articleTagFirefox4, asideTagFirefox4, headerTagFirefox4, footerTagFirefox4, svgTagFirefox4, inputTagFirefox4, datalistTagFirefox4]);
 
     var progressTagFirefox16 = new Tag('progress', [], true);
     var meterTagFirefox16 = new Tag('meter', [], true);
     var firefox16 = new BrowserVersion('16.0', [progressTagFirefox16, meterTagFirefox16]);
 
+    var anchorTagFirefox20 = new Tag('a', [new TagAttribute('download', [], true)], false);
+    var firefox20 = new BrowserVersion('20.0', anchorTagFirefox20);
+
     var videoTagFirefox21 = new Tag('video', [new TagAttribute('type', [new TagAttributeValue('video/mp4', true)], false)], false);
     var audioTagFirefox21 = new Tag('audio', [new TagAttribute('type', [new TagAttributeValue('audio/mpeg', true)], false)], false);
     var firefox21 = new BrowserVersion('21.0', [videoTagFirefox21, audioTagFirefox21]);
 
+    var templateTagFirefox22 = new Tag('template', [], true);
+    var firefox22 = new BrowserVersion('22.0', [templateTagFirefox22]);
+
     var inputTagFirefox23 = new Tag('input', [new TagAttribute('type', [new TagAttributeValue('range', true)], false)], false);
     var firefox23 = new BrowserVersion('23.0', [inputTagFirefox23]);
 
-    var firefox = new Browser('Mozilla Firefox', [firefox2, firefox3_5, firefox4, firefox16, firefox21, firefox23]);
+    var inputTagFirefox29 = new Tag('input', [new TagAttribute('type', [new TagAttributeValue('number', true), new TagAttributeValue('color', true)], false)], false);
+    var firefox29 = new BrowserVersion('29.0', [inputTagFirefox29]);
+
+    var firefox = new Browser('Mozilla Firefox', [firefox2, firefox3_5, firefox4, firefox16, firefox20, firefox21, firefox22, firefox23, firefox29]);
 
 
     //Google Chrome
@@ -361,7 +294,7 @@ var TagAttributeValue = function(value, isNew) {
                     new TagAttributeValue('audio/wav', true)],
                 true)],
         true);
-    var inputTagChrome4 = new Tag('input', new TagAttribute('placeholder',[], true), false);
+    var inputTagChrome4 = new Tag('input', [new TagAttribute('placeholder',[], true)], false);
     var chrome4 = new BrowserVersion('4.0', [canvasTagChrome4, videoTagChrome4, audioTagChrome4, inputTagChrome4]);
 
     var inputTagChrome5 = new Tag('input', [new TagAttribute('type', [new TagAttributeValue('range', true)], false)], false);
@@ -374,16 +307,72 @@ var TagAttributeValue = function(value, isNew) {
     var footerTagChrome6 = new Tag('footer', [], true);
     var chrome6 = new BrowserVersion('6.0', [sectionTagChrome6, articleTagChrome6, asideTagChrome6, headerTagChrome6, footerTagChrome6]);
 
-    var svg = new Tag('svg', [], true);
-    var chrome7 = new BrowserVersion('7.0', [svg]);
+    var svgTagChrome7 = new Tag('svg', [], true);
+    var inputTagChrome7 = new Tag('input', [new TagAttribute('type', [new TagAttributeValue('number', true)], false)], false);
+    var chrome7 = new BrowserVersion('7.0', [svgTagChrome7, inputTagChrome7]);
 
     var progressTagChrome8 = new Tag('progress', [], true);
     var meterTagChrome8 = new Tag('meter', [], true);
     var chrome8 = new BrowserVersion('8.0', [progressTagChrome8, meterTagChrome8]);
 
-    var chrome = new Browser('Google Chrome', [chrome4, chrome5, chrome6, chrome7, chrome8]);
+    var inputTagChrome10 = new Tag('input', [new TagAttribute('required', [], true)], false);
+    var chrome10 = new BrowserVersion('10.0', [inputTagChrome10]);
+
+    var detailsTagChrome12 = new Tag('details', [], true);
+    var summaryTagChrome12 = new Tag('summary', [], true);
+    var chrome12 = new BrowserVersion('12.0', [detailsTagChrome12, summaryTagChrome12]);
+
+    var anchorTagChrome14 = new Tag('a', [new TagAttribute('download', [], true)], false);
+    var chrome14 = new BrowserVersion('14.0', [anchorTagChrome14]);
+
+    var datalistTagChrome20 = new Tag('datalist', [], true);
+    var inputTagChrome20 = new Tag('input', [new TagAttribute('type', [new TagAttributeValue('color', true)], false)], false);
+    var chrome20 = new BrowserVersion('20.0', [datalistTagChrome20, inputTagChrome20]);
+
+    var templateTagChrome26 = new Tag('template', [], true);
+    var chrome26 = new BrowserVersion('26.0', [templateTagChrome26]);
+
+    var imageTagChrome34 = new Tag('img', [new TagAttribute('srcset', [], true)], false);
+    var chrome34 = new BrowserVersion('34.0', [imageTagChrome34]);
+
+    var chrome = new Browser('Google Chrome', [chrome4, chrome5, chrome6, chrome7, chrome8, chrome10, chrome12, chrome14, chrome20, chrome26, chrome34]);
 
 
-
+    //Analyzer initialization
     var htmlCompatibilityAnalyzer = new Analyzer([ie, firefox, chrome]);
-})(window);
+
+    console.log('ie name: ' + ie._name);
+    console.log('is audio in ie9: ' + ie9.isSupportFromThisVersion('auDIO'));
+    console.log('ie versions: ' +ie.getVersionsIdentifiers());
+    console.log('ie tags: ' + ie.getTags());
+    console.log('when canvas supported in ie: ' + ie.whenSupportAddedFor('canvas'));
+
+    //TODO: print where is the tag supported
+
+    var tags = htmlCompatibilityAnalyzer.getDocumentTags();
+    console.log('your website can work in lowest version of IE (all tags):' + ie.getLowestSupportedVersionForAllTags(tags));
+    console.log('your website can work in lowest version of IE (known tags only):' + ie.getLowestSupportedVersionForKnownTags(tags));
+
+    for (var tagId = 0 ; tagId < tags.length; tagId++) {
+        console.log(tags[tagId]);
+    }
+
+    var browsersInfo = htmlCompatibilityAnalyzer.getBrowserSupportInfo();
+
+    console.log('This page is available in these browsers:');
+    for (var browserInfoId = 0; browserInfoId < browsersInfo.length; browserInfoId++) {
+        console.log(browsersInfo[browserInfoId]._name + ' ' + browsersInfo[browserInfoId]._version + '+');
+    }
+
+    var tagsSupportInfo = htmlCompatibilityAnalyzer.getBrowserSupportInfoForEachTag();
+    for (var id = 0; id < tagsSupportInfo.length; id++) {
+        var browserInfo = '|';
+
+        for (var browserId = 0; browserId < tagsSupportInfo[id]._browsersInfo.length; browserId++) {
+            browserInfo += tagsSupportInfo[id]._browsersInfo[browserId]._name + ' ' + tagsSupportInfo[id]._browsersInfo[browserId]._version + '|';
+        }
+        console.log(tagsSupportInfo[id]._tag + ': ' + browserInfo);
+
+
+    }
+})();
